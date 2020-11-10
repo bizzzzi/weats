b9e712f1017451791e4252ad5af27dd4417329e5
csy <gultree99@naver.com>
2020-11-10 16:21:46 +0900

    파트너 마이페이지

--- src/com/partner/PartnerMypageSelectServlet.java
+++ src/com/partner/PartnerMypageSelectServlet.java
@@ -10,29 +10,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.dto.LeportsDTO;
+import com.dto.MemberDTO;
+import com.dto.PartnerDTO;
 import com.service.PartnerService;
 
 /**
- * Servlet implementation class ProductControlServlet
+ * Servlet implementation class PartnerMypageSelectServelt
  */
-@WebServlet("/ProductControlServlet")
-public class ProductControlServlet extends HttpServlet {
+@WebServlet("/PartnerMypageSelectServlet")
+public class PartnerMypageSelectServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			HttpSession session=request.getSession();
 			
-			String leports_id=request.getParameter("leports_id");
-			System.out.println(leports_id);
-		
-			PartnerService pservice=new PartnerService();
-			LeportsDTO ldto=pservice.reservationControl("L9");
-			System.out.println(ldto);
+			MemberDTO dto=(MemberDTO)session.getAttribute("login");
+			 String nextPage = null;
+			 
+			if(dto!=null) {			
+				String user_id=dto.getUser_id();
+				
+				PartnerService pservice=new PartnerService();
+				
+				PartnerDTO pdto=pservice.partnerMypageSelect(user_id);
+				System.out.println(pdto);
+				session.setAttribute("partner", pdto);
+						
+				nextPage="partner/partnerMypage.jsp";
+			}else {
+				System.out.println("dto==null");
+				nextPage="main.jsp";
+			}
+			RequestDispatcher dis = request.getRequestDispatcher(nextPage);
+		      dis.forward(request, response);
 			
-			session.setAttribute("reserve", ldto);
-			
-			RequestDispatcher dis=request.getRequestDispatcher("");
-			dis.forward(request, response);
 		}
 
 	/**
